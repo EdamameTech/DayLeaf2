f4bf7f102910e49507faf6f0e783100193a6fa90
zunda <zundan@gmail.com>
2015-05-25 09:55:58 -1000

    Initialize EditText in onCreate rather than in onStart

--- app/src/main/java/com/edamametech/android/DayLeaf2/TextEditActivity.java
+++ app/src/main/java/com/edamametech/android/DayLeaf2/TextEditActivity.java
@@ -116,11 +116,6 @@ public class TextEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_edit);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
 
         mTextDate = new TextDate(new Date());
         mEditText = (EditText) findViewById(R.id.edit_text);
